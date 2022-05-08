9325d561459be7b2d5527446442e625889ae54a7
Kinolita <55446272+Kinolita@users.noreply.github.com>
2022-05-08 12:41:50 +0200

    Add files via upload

--- streamlit_main.py
+++ streamlit_main.py
@@ -7,10 +7,26 @@ import backend as be
 import utils as utils
 
 
-st.title('Welcome to the project website for the course Social graphs and interactions (02805)')
-st.subheader('Made by Adrienne, Usama, and Ferenc')
+st.markdown("<h1><center>Welcome to the project website for <br> Social Data Analysis and Visualization (02806)</center></h1>",
+            unsafe_allow_html=True)
 
-st.write('With the reduction of rainforests and the ozone layer and the increasingly drastic changes in our climate, the world found it important to create an international treaty in 2016 called the Paris Agreement. This agreement, which was adopted by 196 countries, aims to limit global warming by controlling and reducing greenhouse gas emissions as soon as possible. While the actions and progress of each country are not required to be transparent until 2024, low and zero-carbon solutions and carbon neutrality targets are becoming much more common. But while it appears that countries are making an effort to honor the agreements and work towards carbon neutrality, what is the actual progress to date?')
+with st.container():
+    n1, n2, n3 = st.columns(3)
+    with n1:
+        st.markdown("<h3><center> Adrienne Cohrt <br> s184426 </center></h3>", unsafe_allow_html=True)
+
+    with n2:
+        st.markdown("<h3><center> Ferenc Fodor <br> s220356 </center></h3>", unsafe_allow_html=True)
+
+    with n3:
+        st.markdown("<h3><center> Usama Mir <br> s134187 </center></h3>", unsafe_allow_html=True)
+
+"""With the reduction of rainforests and the ozone layer and the increasingly drastic changes in our climate, the world found it important 
+to create an international treaty in 2016 called the Paris Agreement. This agreement, which was adopted by 196 countries, aims to limit global 
+warming by controlling and reducing greenhouse gas emissions as soon as possible. While the actions and progress of each country are not required to 
+be transparent until 2024, low and zero-carbon solutions and carbon neutrality targets are becoming much more common. But while it appears that 
+countries are making an effort to honor the agreements and work towards carbon neutrality, what is the actual progress to date?"""
+
 st.write("Visulization or table here for the countries? idk")
 
 with st.expander("Webpage parameters", expanded=True):
@@ -32,33 +48,43 @@ with st.expander("Webpage parameters", expanded=True):
 
 st.write("##")
 
-st.write("Below we take a look at the distribution of co2 emissions as it was in 2016. Emissions can be broken into three major categories of impact: industry, buildings, and transport.")
+
 with st.container():
-    col_t1, col_t2 = st.columns([2, 1])
+    col_t1, col_t2 = st.columns([2,1])
 
     with col_t1:
         be.create_emission_pie()
 
     with col_t2:
-        st.write("> Source of CO2 emissions in %")
-
+        st.write("Here we can see the distribution of CO2 emissions in the world in 2016. The major contributors can be broken into three categories of impact: industry, buildings, and transport. Later on we will take a look at how these individual categories have progressed and where they stand today.")
+    
 
 with st.container():
     col_t1, col_t2 = st.columns([1, 2])
     x = 'fossil_share_energy'
     y = 'co2'
     with col_t1:
+        """
+        Of course some countries will be larger contributers of greenhouse gasses than others due to their industry level or population. 
+        These countries will be under a natural spotlight to imporve as they will have the largest contribution to the reduction of 
+        global warming but they should not be expected to carry the responsibility of this for the entire world. Every little bit helps. 
+        Here we can see the amount of carbon dioxide emissions represented by sized boxes for each country and continent. 
+        The color of each box indicates how much of those emissions are the result of fossil fuels.
+        """
         st.write(f">Size of the box: {utils.get_label(be.LABELS, y)}")
         st.write(f">Coloring: {utils.get_label(be.LABELS, x)}")
     with col_t2:
         be.create_tree_plot(x=x, y=y)
 
 
+    """
+    SOo..... From here we could look at the 3 categories (if possible to make visualizations of that) and see how they go renewable% wise from 2016 onwards.
 
+    """
 
 
     col2, col3 = st.columns(2)
-    #
+    
     with col2:
         col2.header('Geo-Scatterplot')
 
@@ -87,20 +113,4 @@ with st.container():
         pass
     with col_p3:
         be.create_lineplot_change(y=y_selected, current_year=2020, window_size=10)
-
-
-
-
-
-    # with col_p2:
-    #     create_scattermap_plot(df_co2, y=y_selected)
-
-    # with col_p3:
-    #     create_line_plot(df_co2, y=y_selected, country_filt=countries_selected)
-    """
-    Speaking about roller coaster rides… The Friends viewers have also been on a bit of a ride.
-    The episode with the lowest overall sentiment was Episode 1 in Season 4: _The one with the Jellyfish_, as you probably remember.
-    The crazy thing is that in the same season we have one of the highest sentiment scores of an episode - Episode 5: _The one with Joey's new girlfriend_.
-    """
-
 
