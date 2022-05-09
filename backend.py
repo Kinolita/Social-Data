8f35240d677fce2ca9f81f9050552fef4986361f
Ferko Fodor <ferkofodor@yahoo.com>
2022-05-09 13:13:29 +0200

    updates to the fuel chart

--- backend.py
+++ backend.py
@@ -263,12 +263,23 @@ def create_tree_plot_window(x, y, year, window_size, hover, reverse=False):
 
 
 def create_energy_consumption_source():
+    labels = ['biofuel_consumption', 'coal_consumption', 'gas_consumption', 'hydro_consumption', 'nuclear_consumption', 'oil_consumption', 'other_renewable_consumption', 'solar_consumption', 'wind_consumption']
     _df = df[['year', 'biofuel_consumption', 'coal_consumption', 'country', 'gas_consumption', 'hydro_consumption', 'nuclear_consumption', 'oil_consumption', 'other_renewable_consumption', 'solar_consumption', 'wind_consumption']].dropna()
     _df = _df[(_df['country'] != 'World') & (_df['country'] != 'World') & (_df['country'] != 'Asia Pacific') & (_df['country'] != 'OECD') & (_df['country'] != 'CIS') & (_df['country'] != 'Middle East') & (_df['country'] != 'Non-OECD')]
     _df = _df.drop(['country'], axis=1)
     _df = _df.groupby('year').sum().reset_index()
-    _df = pd.melt(_df, id_vars=['year'], value_vars=['biofuel_consumption', 'coal_consumption', 'gas_consumption', 'hydro_consumption', 'nuclear_consumption', 'oil_consumption', 'other_renewable_consumption', 'solar_consumption', 'wind_consumption'])
+    _df.columns = ['year'] + [f"{('_'.join(x.split('_')[:-1])).capitalize()}" for x in _df.columns.to_list()[1:]]
+    _df = pd.melt(_df, id_vars=['year'], value_vars=_df.columns.to_list()[1:])
     fig = px.area(_df, x="year", y="value", color="variable", line_group="variable")
-    fig.show()
+    fig.update_layout(
+        hovermode="x unified",
+        xaxis_title="Year",
+        yaxis_title="Energy Consumption (TW/h)",
+        legend_title='Source',
+        legend_traceorder="reversed"
+    )
+    fig.update_traces(hovertemplate='%{y:.2f} TW/h')
+
+    return st.plotly_chart(format_labels(fig), use_container_width=True)
 
 
